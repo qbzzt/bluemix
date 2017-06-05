209789d15017439873fb52e4037a0407854a22c7
qbzzt <qbzzt1@gmail.com>
2017-06-04 19:54:33 -0500

    Create panels.js

---
+++ openwhisk/201706/actions/panels.js
@@ -0,0 +1,20 @@
+
+function main() {
+    // Store the HTML to send in this variable
+    var html = "";
+ 
+    // The two main panels
+    html = '<div class="col-md-3" id="side"></div>' +
+        '<div class="col-md-9" id="main"></div>';
+     
+    // The row-fluid
+    html = '<div class="row-fluid">' + html + "</div>";
+ 
+    // The container-fluid
+    html = '<div class="container-fluid">' + html + "</div>";
+
+    return {
+        "html": html,
+        "js": '$scope.getHtml("side", "sidepanel"); $scope.getHtml("main", "mainpanel");'
+    };
+}
