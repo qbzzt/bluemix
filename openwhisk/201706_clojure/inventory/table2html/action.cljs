63af543d9fa49b58bdb5c7142d95992198676c98
qbzzt <qbzzt1@gmail.com>
2017-06-22 20:12:22 -0500

    Just need to create the actual header and footer

--- openwhisk/201706_clojure/inventory/table2html/action.cljs
+++ openwhisk/201706_clojure/inventory/table2html/action.cljs
@@ -1,24 +1,16 @@
 (ns action.core (:require clojure.string))
 
+(def header (js* "require('fs').readFileSync(__dirname + '/../../../header')"))
+(def footer (js* "require('fs').readFileSync(__dirname + '/../../../footer')"))
 
 
 (defn cljsMain [params] (
     let [
       cljParams (js->clj params)
       data (get cljParams "data")
-      dataKeys (keys data)
-      rowsAsList (map #(clojure.string/join ["<tr><td>" %
-                                    "</td><td>" (get data %) "</td></tr>"])
-                      dataKeys)
-      rowsAsString (clojure.string/join rowsAsList)
-
+      html (get data "html")
     ]
 
-    {"html" (clojure.string/join
-        ["<table><tr><th>Item</th><th>Amt. in Stock</th></tr>"
-        rowsAsString
-        "</table>"
-        ])
-    }
+    {"html" (clojure.string/join [header html footer])}
   )
 )
