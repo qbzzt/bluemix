1794098c9317bb4e232589d47fecd04b102beeeb
qbzzt <qbzzt1@gmail.com>
2017-07-11 00:24:02 -0500

    Can read/write database

--- openwhisk/201706_clojure/article_3/dbase/action.cljs
+++ openwhisk/201706_clojure/article_3/dbase/action.cljs
@@ -1,37 +1,77 @@
 (ns action.core)
 
+(def cloudant-fun (js/require "cloudant"))
 
-(def dbase {
-  "T-shirt XL" 10
-  "T-shirt L" 50
-  "T-shirt M" 0
-  "T-shirt S" 12
-  "T-shirt XS" 0
-  })
+(def cloudant (cloudant-fun <<URL GOES HERE>>))
+
+(def mydb (.use (aget cloudant "db") "openwhisk_inventory"))
+
+
+
+; Process an action with its parameters and the existing database
+; return the result of the action
+(defn processDB [action dbase data]
+  (case action
+    "getAll" {"data" dbase}
+
+    "getAvailable" {"data" (into {} (filter #(> (nth % 1) 0) dbase))}
+
+    "processCorrection" (do
+      (def dbaseNew (into dbase data))
+      {"data" dbaseNew}
+    )
+
+    "processPurchase" (do
+      (def dbaseNew (merge-with #(- %1 %2) dbase data))
+      {"data" dbaseNew}
+    )
+
+    "processReorder" (do
+      (def dbaseNew (merge-with #(+ (- %1 0) (- %2 0)) dbase data))
+      {"data" dbaseNew}
+    )
+
+    {"error" "Unknown action"}
+  )   ;  end of case
+)   ; end of processDB
+
+
 
 (defn cljsMain [params] (
     let [
       cljParams (js->clj params)
       action (get cljParams "action")
       data (get cljParams "data")
+      updateNeeded (or (= action "processReorder")
+                       (= action "processPurchase")
+                       (= action "processCorrection"))
     ]
 
-    (case action
-      "getAll" {"data" dbase}
-      "getAvailable" {"data" (into {} (filter #(> (nth % 1) 0) dbase))}
-      "processCorrection" (do
-          (def dbase (into dbase data))
-          {"data" dbase}
-      )
-      "processPurchase" (do
-          (def dbase (merge-with #(- %1 %2) dbase data))
-          {"data" dbase}
-      )
-      "processReorder" (do
-          (def dbase (merge-with #(+ (cljs.reader/parse-int %1) (cljs.reader/parse-int %2)) dbase data))
-          {"data" dbase}
-      )
-      {"error" "Unknown action"}
+    ; Because promise-resolve is here, it can reference
+    ; action
+    (defn promise-resolve [resolve param] (let
+      [
+        dbaseJS (aget param "dbase")
+        dbaseOld (js->clj dbaseJS)
+        result (processDB action dbaseOld data)
+        rev (aget param "_rev")
+      ]
+        (if updateNeeded
+          (.insert mydb (clj->js {"dbase" (get result "data"),
+                                  "_id" "dbase",
+                                  "_rev" rev})
+            #(do (prn result) (prn (get result "data")) (resolve (clj->js result)))
+          )
+          (resolve (clj->js result))
+        )
+      )   ; end of let
+    )   ; end of defn promise-resolve
+
+
+    (defn promise-func [resolve reject]
+      (.get mydb "dbase" #(promise-resolve resolve %2))
     )
-  )
-)
+
+    (js/Promise. promise-func)
+  )   ; end of let
+)    ; end of cljsMain
