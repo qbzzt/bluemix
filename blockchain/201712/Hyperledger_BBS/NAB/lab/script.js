1e330f8a3007b52ccbaa1316223ca92c54ebef86
qbzzt <qbzzt1@gmail.com>
2017-11-26 17:11:25 -0600

    Create script.js

---
+++ blockchain/201712/Hyperledger_BBS/NAB/lab/script.js
@@ -0,0 +1,7 @@
+/**
+ * Sample transaction processor function.
+ * @param {com.ibm.developerworks.orip.bbs.Message} tx The sample transaction instance.
+ * @transaction
+ */
+function message(tx) {
+}
