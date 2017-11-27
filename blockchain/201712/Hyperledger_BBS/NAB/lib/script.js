c980858c601664b193ff3913a8caba44c6dd932e
qbzzt <qbzzt1@gmail.com>
2017-11-26 20:30:12 -0600

    Delete script.js

--- blockchain/201712/Hyperledger_BBS/NAB/lib/script.js
+++
@@ -1,7 +0,0 @@
-/**
- * Sample transaction processor function.
- * @param {com.ibm.developerworks.orip.bbs.Message} tx The sample transaction instance.
- * @transaction
- */
-function message(tx) {
-}
