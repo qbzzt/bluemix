6c17aac9d1de5f1f5619eac334085b124e0c6b78
qbzzt <qbzzt1@gmail.com>
2018-11-10 13:19:12 -0600

    Create 03_NodeMCU_w_usb.js

---
+++ 3d/201809/blog/03_NodeMCU_w_usb.js
@@ -0,0 +1,28 @@
+
+const devSize = [49, 24.5, 13];
+
+const margin = 3;
+const wallWidth = 10;
+
+const usbHoleSize = [wallWidth, 8, 5];
+
+
+function main () {
+    const holeSize = devSize.map(l => l+2*margin);
+    const boxSize = [
+        holeSize[0]+2*wallWidth, 
+        holeSize[1]+2*wallWidth,
+        holeSize[2]+wallWidth,        
+    ];
+    
+    const box = cube({size: boxSize});
+    const hole = cube({size:holeSize}).
+        translate([wallWidth, wallWidth, wallWidth]);
+        
+    const usbHole = cube({size: usbHoleSize}).
+        translate([0,(boxSize[1]-usbHoleSize[1])/2,wallWidth]);        
+    
+    const completeBox = difference(box, hole, usbHole);
+
+    return completeBox;
+}
